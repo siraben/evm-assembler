92d15c907f06ad41717049eb62734208de00a5d8
Ben Siraphob <bensiraphob@gmail.com>
2020-06-22 15:14:52 +0700

    Comment contract.lisp

--- demo/contract.lisp
+++ demo/contract.lisp
@@ -1,13 +1,20 @@
-;; PUSH1 0 CALLDATALOAD SLOAD NOT PUSH1 10 JUMPI STOP JUMPDEST PUSH1 32 CALLDATALOAD PUSH1 0 CALLDATALOAD SSTORE
-0 calldataload
-sload not
-(jumpi foo)
-stop
-(label foo)
-32
-calldataload
-0
-calldataload
+;; Eaxmple contract from
+;; https://github.com/ethereum/wiki/wiki/Ethereum-Development-Tutorial
 
+;; PUSH1 0 CALLDATALOAD SLOAD NOT PUSH1 10 JUMPI STOP JUMPDEST PUSH1
+;; 32 CALLDATALOAD PUSH1 0 CALLDATALOAD SSTORE
+0 calldataload
+sload
+;; If key not found in store
+not
+(jumpi write_store)
+;; Already in store, push 10 to indicate so.
+10
+stop
+(label write_store)
+;; Get the value, key
+32 calldataload
+0 calldataload
+;; Store it
 sstore
 stop
