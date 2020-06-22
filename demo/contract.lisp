bd3e962ddb8906bbf0095227e254efc0968aba53
Ben Siraphob <bensiraphob@gmail.com>
2020-06-22 16:00:18 +0700

    Add contract creation

--- demo/contract.lisp
+++ demo/contract.lisp
@@ -1,20 +1,18 @@
-;; Eaxmple contract from
+;; Example contract from
 ;; https://github.com/ethereum/wiki/wiki/Ethereum-Development-Tutorial
 
 ;; PUSH1 0 CALLDATALOAD SLOAD NOT PUSH1 10 JUMPI STOP JUMPDEST PUSH1
 ;; 32 CALLDATALOAD PUSH1 0 CALLDATALOAD SSTORE
 0 calldataload
 sload
-;; If key not found in store
-not
-(jumpi write_store)
-;; Already in store, push 10 to indicate so.
-;; 10
+;; If the key is not in storage, jump to write it.
+not (jumpi write_storage)
+;; Already in storage, stop.
 stop
-(label write_store)
-;; Get the value, key
+(label write_storage)
+;; Get the value, key.
 32 calldataload
 0 calldataload
-;; Store it
+;; Store it.
 sstore
 stop
---
+++ demo/create_contract.lisp
@@ -0,0 +1,26 @@
+;; PUSH1 17 DUP1 PUSH1 12 PUSH1 0 CODECOPY PUSH1 0 RETURN STOP PUSH1 0
+;; CALLDATALOAD SLOAD NOT PUSH1 9 JUMPI STOP JUMPDEST PUSH1 32
+;; CALLDATALOAD PUSH1 0 CALLDATALOAD SSTORE
+19
+dup
+12
+0
+codecopy
+push1
+0
+return
+stop
+0
+calldataload
+sload
+not
+9
+(jumpi foo)
+stop
+(label foo)
+32
+calldataload
+0
+calldataload
+sstore
+
