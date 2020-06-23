c9a41e10ad67929c0b6be351545740eec3187f2b
Viacheslav Litvinov <viacheslav@hot-now.com>
2020-06-23 14:38:09 +0700

    Change bitwise NOT to ISZERO

--- demo/create_contract.lisp
+++ demo/create_contract.lisp
@@ -1,7 +1,7 @@
 ;; PUSH1 17 DUP1 PUSH1 12 PUSH1 0 CODECOPY PUSH1 0 RETURN STOP PUSH1 0
 ;; CALLDATALOAD SLOAD NOT PUSH1 9 JUMPI STOP JUMPDEST PUSH1 32
 ;; CALLDATALOAD PUSH1 0 CALLDATALOAD SSTORE
-19
+30
 dup
 contract-start
 1 + ;; HACK: adjust the address
@@ -16,7 +16,7 @@ stop
 0
 calldataload
 sload
-not
+iszero
 (jumpi foo)
 stop
 (label foo)
