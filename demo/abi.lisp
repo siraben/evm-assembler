f1b715073d93b24493d6cb0cb09c4f935ae3eb6d
Ben Siraphob <bensiraphob@gmail.com>
2020-06-23 15:52:57 +0700

    Add ABI demo

---
+++ demo/abi.lisp
@@ -0,0 +1,23 @@
+100
+dup
+contract-start
+1 + ;; HACK: adjust the address
+0
+codecopy
+0
+return
+stop
+;; Contract starts
+(label contract-start)
+(org 0)
+0 calldataload
+28 rshift
+;; 0xcdcd77c0
+3452794816 = (jumpi baz)
+stop
+(label baz)
+4 calldataload  ;; Load x : u32
+0 sstore
+36 calldataload ;; Load y : bool
+1 sstore
+stop
