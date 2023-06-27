3c0eeae41151fa6a688ff7060c676a04318ebb8e
Ben Siraphob <bensiraphob@gmail.com>
2023-06-28 00:06:50 +0700

    Add stack stress test

---
+++ examples/stack.lisp
@@ -0,0 +1,25 @@
+;; Program to do a quadratic number of operations on the stack.
+
+1019 0
+
+(label main)
+over
+(label push-n)
+dup 0= (jumpi end)
+dup 1 - (jump push-n)
+
+(label end)
+drop
+(label clear)
+dup 0= (jumpi end2)
+drop
+(jump clear)
+
+(label end2)
+over 0= (jumpi finish)
+swap 1 - swap
+(jump main)
+
+(label finish)
+stop
+
